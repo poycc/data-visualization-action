e2a439903ffb99dce23fef6c02bc50cd8f955cdf
anddi <zhufind@yeah.net>
2020-01-11 12:50:42 +0800

    ErrorBoundary

---
+++ src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+interface IProps {
+  children: React.ReactNode;
+}
+interface IState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  // componentDidCatch(error, errorInfo) {
+  //   // You can also log the error to an error reporting service
+  //   logErrorToMyService(error, errorInfo);
+  // }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+    if (hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return children;
+  }
+}
+
+export default ErrorBoundary;
