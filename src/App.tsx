7b40fa70a2339afa19282bad261f071e2a4537d4
anddi <zhufind@yeah.net>
2020-01-12 23:00:32 +0800

    refactor(app): rename Basiclayout to BasicLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import BasicLayout from './components/Basiclayout';
+import BasicLayout from './components/BasicLayout';
 
 const App: React.FC = () => (
   <BrowserRouter>
