43825573cdfaf24d7546353ce1be441a92c9eb87
anddi <zhufind@yeah.net>
2020-01-14 13:57:28 +0800

    use arc() draw arcs and circles

--- src/pages/Canvas/Basic/BasicList/index.tsx
+++ src/pages/Canvas/Basic/BasicList/index.tsx
@@ -47,10 +47,30 @@ const BasicList = () => {
       ctx.lineTo(660, 10);
       ctx.closePath();
       ctx.fill();
+
+      for (let i = 0; i < 4; i += 1) {
+        for (let j = 0; j < 3; j += 1) {
+          ctx.beginPath();
+          const x = 700 + j * 50;
+          const y = 50 + i * 50;
+          const radius = 20;
+          const startAngle = 0;
+          const endAngle = Math.PI + (Math.PI * j) / 2;
+          const anticlockwise = i % 2 !== 0;
+
+          ctx.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+          if (i > 1) {
+            ctx.fill();
+          } else {
+            ctx.stroke();
+          }
+        }
+      }
     }
   }, []);
 
-  return <canvas width={1200} height={800} ref={canvasRef} />;
+  return <canvas width={1400} height={800} ref={canvasRef} />;
 };
 
 export default BasicList;
