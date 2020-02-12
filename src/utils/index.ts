e0f82917f34b20622c5d9a9c164b99ebabbfe85c
poycc <zhufind@yeah.net>
2020-02-12 17:44:25 +0800

    refactor(setCanvas tool function):

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,16 @@
-export const setupCanvas = (canvas: HTMLCanvasElement) => {
+export const setupCanvas: (
+  canvas: HTMLCanvasElement,
+) => CanvasRenderingContext2D | null = (canvas: HTMLCanvasElement) => {
   const dpr = window.devicePixelRatio || 1;
   const rect = canvas.getBoundingClientRect();
   const { width, height } = rect;
+  /* eslint no-param-reassign: ["error", { "props": false }] */
   canvas.style.width = `${width}px`;
   canvas.style.height = `${height}px`;
   canvas.width = rect.width * dpr;
   canvas.height = rect.height * dpr;
   const ctx = canvas.getContext('2d');
-  if (!ctx) return;
+  if (!ctx) return null;
   ctx.scale(dpr, dpr);
   return ctx;
 };
